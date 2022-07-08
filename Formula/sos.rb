ab60aca2dfe0fa4237bd0db9a835b302b362237a
muji <muji@tmpfs.org>
2022-07-08 10:06:28 +0800

    Draft formula.

---
+++ Formula/sos.rb
@@ -0,0 +1,24 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sos < Formula
+  desc "Distributed, encrypted database for private secrets."
+  homepage "https://github.com/saveoursecrets/library"
+  url "http://localhost:8000/sos-tools-macos-release-0.1.0.tar.gz"
+  sha256 "2c4b042ec960f8ed62a8e3748c5f74a4b1a738194b079e6838c4841d6b3e9d56"
+  version "0.1.0"
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "arm/sos-audit"
+      bin.install "arm/sos-check"
+      bin.install "arm/sos-client"
+      bin.install "arm/sos-server"
+    else
+      bin.install "x86/sos-audit"
+      bin.install "x86/sos-check"
+      bin.install "x86/sos-client"
+      bin.install "x86/sos-server"
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+# Homebrew Formula
+
+Formula for installing the Save our Secrets command line tools for MacOS.
