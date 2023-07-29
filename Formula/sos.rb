34c2fa5eca28278703035fb4f1806e6ae55735ca
muji <muji@tmpfs.org>
2023-07-29 09:58:47 +0800

    Update install def.

--- Formula/sos.rb
+++ Formula/sos.rb
@@ -7,10 +7,6 @@ class Sos < Formula
   homepage "https://saveoursecrets.com"
   license "GPL-3.0"
 
-  bin.install "sos"
-  bin.install "sos-server"
-  bin.install "sos-rendezvous"
-
   if Hardware::CPU.arm?
     sha256 "b7cc04b183bb63918434d7f39d9fa31aabe4ec481cc455bd39b3f9bf27ea7f02"
     url "https://releases.saveoursecrets.com/cli/macos/0.5.1/aarch64/saveoursecrets.zip"
@@ -19,4 +15,9 @@ class Sos < Formula
     url "https://releases.saveoursecrets.com/cli/macos/0.5.1/x86_64/saveoursecrets.zip"
   end
 
+  def install
+    bin.install "sos"
+    bin.install "sos-server"
+    bin.install "sos-rendezvous"
+  end
 end
