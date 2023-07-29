b6afee078c5fc8334a242ddf50b8b9c98071c420
muji <muji@tmpfs.org>
2023-07-29 09:51:48 +0800

    Update formula.

--- Formula/sos.rb
+++ Formula/sos.rb
@@ -4,15 +4,15 @@
 class Sos < Formula
   version "0.5.1"
   desc "Distributed, encrypted database for private secrets."
-  homepage "https://github.com/saveoursecrets/sdk"
+  homepage "https://saveoursecrets.com"
   license "GPL-3.0"
 
   if Hardware::CPU.arm?
-    sha256 "240812eebd4602c9bbc1d42bab9d00b7f7a3f8b61581aaa735423562969c1c73"
-    url "https://github.com/saveoursecrets/sdk/releases/download/v0.5.1/cli-macos-0.5.1-aarch64-saveoursecrets.zip"
+    sha256 "b7cc04b183bb63918434d7f39d9fa31aabe4ec481cc455bd39b3f9bf27ea7f02"
+    url "https://releases.saveoursecrets.com/cli/macos/0.5.1/aarch64/saveoursecrets.zip"
   else
-    sha256 "6f4035c263161c3388fe9d6b03f0ec6304d51bd527e9bf7986dfb28d9b5a8bb1"
-    url "https://github.com/saveoursecrets/sdk/releases/download/v0.5.1/cli-macos-0.5.1-x86_64-saveoursecrets.zip"
+    sha256 "cd74a1cb232fac2783cc3905836c34a55331fc25289cc806344019557982d92d"
+    url "https://releases.saveoursecrets.com/cli/macos/0.5.1/x86_64/saveoursecrets.zip"
   end
 
 end
